0342b8c6b4f56753052fbe64d9ab467f2e1a43a8
Kevyn Andrade <kevsdeandrade@gmail.com>
2023-11-30 19:06:18 -0300

    adicionando ambiente de execução e parâmetros

--- main.R
+++ main.R
@@ -2,3 +2,10 @@ if (!require(pacman)) install.packages("pacman")
 
 pacman::p_load(simmer, tidyverse)
 
+set.seed(8273)
+
+pedidos_hora <- 12.40 # média de pedidos q chegam por hora. segue poisson
+atendimento_hora <- 2.29  # média de pedidos atendidos por hora 
+tempo_atendimento <- 26.21 # tempo médio de atendimento por pedido
+
+env <- simmer("Restaurante")   # ambiente
